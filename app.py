e9cc821af4189dce376bfbd6073404f5f1d1f96d
Susanna Bernardes <susanna.bernardes@synvia.com>
2026-01-06 16:15:50 -0300

    fix e add: merge inteligente att + login microsoft

--- app.py
+++ app.py
@@ -9,6 +9,16 @@ import string
 # >>> usa o conector (precisa do arquivo sp_connector.py no repo)
 from sp_connector import SPConnector
 
+
+from auth_microsoft import (
+    AuthManager,
+    MicrosoftAuth,
+    create_login_page,
+    create_user_header,
+)
+
+
+
 # === Configurações via secrets (SharePoint - site servicosclinicos) ===
 TENANT_ID = st.secrets["graph"]["tenant_id"]
 CLIENT_ID = st.secrets["graph"]["client_id"]
@@ -72,30 +82,61 @@ def generate_custom_id(existing_ids: set[str]) -> str:
 
 # Função para atualizar o arquivo Excel (Apontamentos) no SharePoint
 def update_sharepoint_file(df: pd.DataFrame) -> pd.DataFrame | None:
+    """
+    Atualiza o arquivo Excel no SharePoint de forma segura, prevenindo sobrescrita de dados.
+
+    Estratégia:
+    1. Carrega a versão mais recente do arquivo
+    2. Para linhas existentes: atualiza APENAS as colunas que foram modificadas
+    3. Para linhas novas: adiciona ao final
+    4. Tenta novamente em caso de conflito de versão
+    """
     attempts = 0
     while True:
         try:
             # Carrega versão mais recente do arquivo
             base_df = _sp().read_excel(APONTAMENTOS)
+
+            df_to_save = df.copy()
+            if "ID" not in df_to_save.columns:
+                st.error("DataFrame sem coluna ID!")
+                return None
+
+            df_to_save["ID"] = df_to_save["ID"].astype(str)
+
             if not base_df.empty:
                 base_df["ID"] = base_df["ID"].astype(str)
-                base_df.set_index("ID", inplace=True)
+
+                # Separa registros novos dos existentes
+                ids_to_save = set(df_to_save["ID"].tolist())
+                existing_ids = set(base_df["ID"].tolist())
+
+                new_ids = ids_to_save - existing_ids
+                update_ids = ids_to_save & existing_ids
+
+                # Adiciona registros completamente novos
+                if new_ids:
+                    new_rows = df_to_save[df_to_save["ID"].isin(new_ids)]
+                    base_df = pd.concat([base_df, new_rows], ignore_index=True)
+
+                # Atualiza registros existentes coluna por coluna
+                for id_val in update_ids:
+                    idx_base = base_df.index[base_df["ID"] == id_val].tolist()
+                    idx_update = df_to_save.index[df_to_save["ID"] == id_val].tolist()
+
+                    if idx_base and idx_update:
+                        idx_b = idx_base[0]
+                        idx_u = idx_update[0]
+
+                        # Atualiza apenas as colunas que existem em ambos
+                        for col in df_to_save.columns:
+                            if col in base_df.columns:
+                                base_df.at[idx_b, col] = df_to_save.at[idx_u, col]
             else:
-                base_df = pd.DataFrame().set_index("ID")
+                # Se o arquivo está vazio, salva tudo
+                base_df = df_to_save.copy()
 
-            df = df.copy()
-            if "ID" in df.columns:
-                df["ID"] = df["ID"].astype(str)
-                df.set_index("ID", inplace=True)
-
-            # Atualiza linhas existentes e adiciona novas
-            base_df.update(df)
-            novos = df.index.difference(base_df.index)
-            if len(novos) > 0:
-                base_df = pd.concat([base_df, df.loc[novos]])
-
-            base_df.reset_index(inplace=True)
-
+            # Salva o arquivo atualizado
             output = io.BytesIO()
             base_df.to_excel(output, index=False)
             output.seek(0)
@@ -103,6 +144,7 @@ def update_sharepoint_file(df: pd.DataFrame) -> pd.DataFrame | None:
 
             st.success("Mudanças submetidas com sucesso! Recarregue a página para ver as mudanças")
             return base_df
+
         except Exception as e:
             attempts += 1
             msg = str(e)
@@ -113,6 +155,28 @@ def update_sharepoint_file(df: pd.DataFrame) -> pd.DataFrame | None:
                 continue
             st.error(f"Erro ao salvar no SharePoint (Graph): {msg}")
             return None
+
+
+# -------------------------------------------------
+# Autenticação e contexto do usuário
+# -------------------------------------------------
+auth = MicrosoftAuth()
+logged_in = create_login_page(auth)
+if not logged_in:
+    st.stop()
+
+# Garantir token válido durante a sessão
+AuthManager.check_and_refresh_token(auth)
+create_user_header()
+
+user = AuthManager.get_current_user() or {}
+display_name = user.get("displayName", "Usuário")
+user_email = (user.get("mail") or user.get("userPrincipalName") or "").lower()
+
+
+st.session_state["display_name"] = display_name
+st.session_state["user_email"] = user_email
+
 
 # Carregar dados iniciais
 df_study = get_sharepoint_file_estudos_csv()
@@ -197,7 +261,7 @@ if "active_tab" not in st.session_state:
     st.session_state.active_tab = tab_names[0]
 
 tab_option = st.radio(
-    label="Navegação",  
+    label="",  
     options=tab_names,
     horizontal=True,
     key="active_tab",
@@ -225,7 +289,6 @@ if tab_option == "Formulário":
             research_name = ""
         st.text_input("Nome da Pesquisa", value=research_name, disabled=True)
         
-        responsavel = st.text_input("Responsável pelo Apontamento", key="responsavel_apontamento")
         
         origem = st.selectbox(
             "Origem Do Apontamento", 
@@ -268,11 +331,13 @@ if tab_option == "Formulário":
         
         # Obtém o valor final do documento usando a função
         documento_final = get_final_documento()
+
+        pp_options = ["N/A", "Outros"] + [f"PP{i:02d}" for i in range(1, 100)] + [f"PP{i}" for i in range(100, 1000)]
+
+        participante = st.selectbox("Participante", pp_options, key="participante")
         
 
-        participante = st.selectbox("Participante", 
-            ['N/A', 'Outros', 'PP01', 'PP02', 'PP03', 'PP04', 'PP05', 'PP06', 'PP07', 'PP08', 'PP09', 'PP10', 'PP11', 'PP12', 'PP13', 'PP14', 'PP15', 'PP16', 'PP17', 'PP18', 'PP19', 'PP20', 'PP21', 'PP22', 'PP23', 'PP24', 'PP25', 'PP26', 'PP27', 'PP28', 'PP29', 'PP30', 'PP31', 'PP32', 'PP33', 'PP34', 'PP35', 'PP36', 'PP37', 'PP38', 'PP39', 'PP40', 'PP41', 'PP42', 'PP43', 'PP44', 'PP45', 'PP46', 'PP47', 'PP48', 'PP49', 'PP50', 'PP51', 'PP52', 'PP53', 'PP54', 'PP55', 'PP56', 'PP57', 'PP58', 'PP59', 'PP60', 'PP61', 'PP62', 'PP63', 'PP64', 'PP65', 'PP66', 'PP67', 'PP68', 'PP69', 'PP70', 'PP71', 'PP72', 'PP73', 'PP74', 'PP75', 'PP76', 'PP77', 'PP78', 'PP79', 'PP80', 'PP81', 'PP82', 'PP83', 'PP84', 'PP85', 'PP86', 'PP87', 'PP88', 'PP89', 'PP90', 'PP91', 'PP92', 'PP93', 'PP94', 'PP95', 'PP96', 'PP97', 'PP98', 'PP99', 'PP100', 'PP101', 'PP102', 'PP103', 'PP104', 'PP105', 'PP106', 'PP107', 'PP108', 'PP109', 'PP110', 'PP111', 'PP112', 'PP113', 'PP114', 'PP115', 'PP116', 'PP117', 'PP118', 'PP119', 'PP120', 'PP121', 'PP122', 'PP123', 'PP124', 'PP125', 'PP126', 'PP127', 'PP128', 'PP129', 'PP130', 'PP131', 'PP132', 'PP133', 'PP134', 'PP135', 'PP136', 'PP137', 'PP138', 'PP139', 'PP140', 'PP141', 'PP142', 'PP143', 'PP144', 'PP145', 'PP146', 'PP147', 'PP148', 'PP149', 'PP150', 'PP151', 'PP152', 'PP153', 'PP154', 'PP155', 'PP156', 'PP157', 'PP158', 'PP159', 'PP160', 'PP161', 'PP162', 'PP163', 'PP164', 'PP165', 'PP166', 'PP167', 'PP168', 'PP169', 'PP170', 'PP171', 'PP172', 'PP173', 'PP174', 'PP175', 'PP176', 'PP177', 'PP178', 'PP179', 'PP180', 'PP181', 'PP182', 'PP183', 'PP184', 'PP185', 'PP186', 'PP187', 'PP188', 'PP189', 'PP190', 'PP191', 'PP192', 'PP193', 'PP194', 'PP195', 'PP196', 'PP197', 'PP198', 'PP199', 'PP200', 'PP201', 'PP202', 'PP203', 'PP204', 'PP205', 'PP206', 'PP207', 'PP208', 'PP209', 'PP210', 'PP211', 'PP212', 'PP213', 'PP214', 'PP215', 'PP216', 'PP217', 'PP218', 'PP219', 'PP220', 'PP221', 'PP222', 'PP223', 'PP224', 'PP225', 'PP226', 'PP227', 'PP228', 'PP229', 'PP230', 'PP231', 'PP232', 'PP233', 'PP234', 'PP235', 'PP236', 'PP237', 'PP238', 'PP239', 'PP240', 'PP241', 'PP242', 'PP243', 'PP244', 'PP245', 'PP246', 'PP247', 'PP248', 'PP249', 'PP250', 'PP251', 'PP252', 'PP253', 'PP254', 'PP255', 'PP256', 'PP257', 'PP258', 'PP259', 'PP260', 'PP261', 'PP262', 'PP263', 'PP264', 'PP265', 'PP266', 'PP267', 'PP268', 'PP269', 'PP270', 'PP271', 'PP272', 'PP273', 'PP274', 'PP275', 'PP276', 'PP277', 'PP278', 'PP279', 'PP280', 'PP281', 'PP282', 'PP283', 'PP284', 'PP285', 'PP286', 'PP287', 'PP288', 'PP289', 'PP290', 'PP291', 'PP292', 'PP293', 'PP294', 'PP295', 'PP296', 'PP297', 'PP298', 'PP299', 'PP300', 'PP301', 'PP302', 'PP303', 'PP304', 'PP305', 'PP306', 'PP307', 'PP308', 'PP309', 'PP310', 'PP311', 'PP312', 'PP313', 'PP314', 'PP315', 'PP316', 'PP317', 'PP318', 'PP319', 'PP320', 'PP321', 'PP322', 'PP323', 'PP324', 'PP325', 'PP326', 'PP327', 'PP328', 'PP329', 'PP330', 'PP331', 'PP332', 'PP333', 'PP334', 'PP335', 'PP336', 'PP337', 'PP338', 'PP339', 'PP340', 'PP341', 'PP342', 'PP343', 'PP344', 'PP345', 'PP346', 'PP347', 'PP348', 'PP349', 'PP350', 'PP351', 'PP352', 'PP353', 'PP354', 'PP355', 'PP356', 'PP357', 'PP358', 'PP359', 'PP360', 'PP361', 'PP362', 'PP363', 'PP364', 'PP365', 'PP366', 'PP367', 'PP368', 'PP369', 'PP370', 'PP371', 'PP372', 'PP373', 'PP374', 'PP375', 'PP376', 'PP377', 'PP378', 'PP379', 'PP380', 'PP381', 'PP382', 'PP383', 'PP384', 'PP385', 'PP386', 'PP387', 'PP388', 'PP389', 'PP390', 'PP391', 'PP392', 'PP393', 'PP394', 'PP395', 'PP396', 'PP397', 'PP398', 'PP399', 'PP400', 'PP401', 'PP402', 'PP403', 'PP404', 'PP405', 'PP406', 'PP407', 'PP408', 'PP409', 'PP410', 'PP411', 'PP412', 'PP413', 'PP414', 'PP415', 'PP416', 'PP417', 'PP418', 'PP419', 'PP420', 'PP421', 'PP422', 'PP423', 'PP424', 'PP425', 'PP426', 'PP427', 'PP428', 'PP429', 'PP430', 'PP431', 'PP432', 'PP433', 'PP434', 'PP435', 'PP436', 'PP437', 'PP438', 'PP439', 'PP440', 'PP441', 'PP442', 'PP443', 'PP444', 'PP445', 'PP446', 'PP447', 'PP448', 'PP449', 'PP450', 'PP451', 'PP452', 'PP453', 'PP454', 'PP455', 'PP456', 'PP457', 'PP458', 'PP459', 'PP460', 'PP461', 'PP462', 'PP463', 'PP464', 'PP465', 'PP466', 'PP467', 'PP468', 'PP469', 'PP470', 'PP471', 'PP472', 'PP473', 'PP474', 'PP475', 'PP476', 'PP477', 'PP478', 'PP479', 'PP480', 'PP481', 'PP482', 'PP483', 'PP484', 'PP485', 'PP486', 'PP487', 'PP488', 'PP489', 'PP490', 'PP491', 'PP492', 'PP493', 'PP494', 'PP495', 'PP496', 'PP497', 'PP498', 'PP499', 'PP500', 'PP501', 'PP502', 'PP503', 'PP504', 'PP505', 'PP506', 'PP507', 'PP508', 'PP509', 'PP510', 'PP511', 'PP512', 'PP513', 'PP514', 'PP515', 'PP516', 'PP517', 'PP518', 'PP519', 'PP520', 'PP521', 'PP522', 'PP523', 'PP524', 'PP525', 'PP526', 'PP527', 'PP528', 'PP529', 'PP530', 'PP531', 'PP532', 'PP533', 'PP534', 'PP535', 'PP536', 'PP537', 'PP538', 'PP539', 'PP540', 'PP541', 'PP542', 'PP543', 'PP544', 'PP545', 'PP546', 'PP547', 'PP548', 'PP549', 'PP550', 'PP551', 'PP552', 'PP553', 'PP554', 'PP555', 'PP556', 'PP557', 'PP558', 'PP559', 'PP560', 'PP561', 'PP562', 'PP563', 'PP564', 'PP565', 'PP566', 'PP567', 'PP568', 'PP569', 'PP570', 'PP571', 'PP572', 'PP573', 'PP574', 'PP575', 'PP576', 'PP577', 'PP578', 'PP579', 'PP580', 'PP581', 'PP582', 'PP583', 'PP584', 'PP585', 'PP586', 'PP587', 'PP588', 'PP589', 'PP590', 'PP591', 'PP592', 'PP593', 'PP594', 'PP595', 'PP596', 'PP597', 'PP598', 'PP599', 'PP600', 'PP601', 'PP602', 'PP603', 'PP604', 'PP605', 'PP606', 'PP607', 'PP608', 'PP609', 'PP610', 'PP611', 'PP612', 'PP613', 'PP614', 'PP615', 'PP616', 'PP617', 'PP618', 'PP619', 'PP620', 'PP621', 'PP622', 'PP623', 'PP624', 'PP625', 'PP626', 'PP627', 'PP628', 'PP629', 'PP630', 'PP631', 'PP632', 'PP633', 'PP634', 'PP635', 'PP636', 'PP637', 'PP638', 'PP639', 'PP640', 'PP641', 'PP642', 'PP643', 'PP644', 'PP645', 'PP646', 'PP647', 'PP648', 'PP649', 'PP650', 'PP651', 'PP652', 'PP653', 'PP654', 'PP655', 'PP656', 'PP657', 'PP658', 'PP659', 'PP660', 'PP661', 'PP662', 'PP663', 'PP664', 'PP665', 'PP666', 'PP667', 'PP668', 'PP669', 'PP670', 'PP671', 'PP672', 'PP673', 'PP674', 'PP675', 'PP676', 'PP677', 'PP678', 'PP679', 'PP680', 'PP681', 'PP682', 'PP683', 'PP684', 'PP685', 'PP686', 'PP687', 'PP688', 'PP689', 'PP690', 'PP691', 'PP692', 'PP693', 'PP694', 'PP695', 'PP696', 'PP697', 'PP698', 'PP699', 'PP700', 'PP701', 'PP702', 'PP703', 'PP704', 'PP705', 'PP706', 'PP707', 'PP708', 'PP709', 'PP710', 'PP711', 'PP712', 'PP713', 'PP714', 'PP715', 'PP716', 'PP717', 'PP718', 'PP719', 'PP720', 'PP721', 'PP722', 'PP723', 'PP724', 'PP725', 'PP726', 'PP727', 'PP728', 'PP729', 'PP730', 'PP731', 'PP732', 'PP733', 'PP734', 'PP735', 'PP736', 'PP737', 'PP738', 'PP739', 'PP740', 'PP741', 'PP742', 'PP743', 'PP744', 'PP745', 'PP746', 'PP747', 'PP748', 'PP749', 'PP750', 'PP751', 'PP752', 'PP753', 'PP754', 'PP755', 'PP756', 'PP757', 'PP758', 'PP759', 'PP760', 'PP761', 'PP762', 'PP763', 'PP764', 'PP765', 'PP766', 'PP767', 'PP768', 'PP769', 'PP770', 'PP771', 'PP772', 'PP773', 'PP774', 'PP775', 'PP776', 'PP777', 'PP778', 'PP779', 'PP780', 'PP781', 'PP782', 'PP783', 'PP784', 'PP785', 'PP786', 'PP787', 'PP788', 'PP789', 'PP790', 'PP791', 'PP792', 'PP793', 'PP794', 'PP795', 'PP796', 'PP797', 'PP798', 'PP799', 'PP800', 'PP801', 'PP802', 'PP803', 'PP804', 'PP805', 'PP806', 'PP807', 'PP808', 'PP809', 'PP810', 'PP811', 'PP812', 'PP813', 'PP814', 'PP815', 'PP816', 'PP817', 'PP818', 'PP819', 'PP820', 'PP821', 'PP822', 'PP823', 'PP824', 'PP825', 'PP826', 'PP827', 'PP828', 'PP829', 'PP830', 'PP831', 'PP832', 'PP833', 'PP834', 'PP835', 'PP836', 'PP837', 'PP838', 'PP839', 'PP840', 'PP841', 'PP842', 'PP843', 'PP844', 'PP845', 'PP846', 'PP847', 'PP848', 'PP849', 'PP850', 'PP851', 'PP852', 'PP853', 'PP854', 'PP855', 'PP856', 'PP857', 'PP858', 'PP859', 'PP860', 'PP861', 'PP862', 'PP863', 'PP864', 'PP865', 'PP866', 'PP867', 'PP868', 'PP869', 'PP870', 'PP871', 'PP872', 'PP873', 'PP874', 'PP875', 'PP876', 'PP877', 'PP878', 'PP879', 'PP880', 'PP881', 'PP882', 'PP883', 'PP884', 'PP885', 'PP886', 'PP887', 'PP888', 'PP889', 'PP890', 'PP891', 'PP892', 'PP893', 'PP894', 'PP895', 'PP896', 'PP897', 'PP898', 'PP899', 'PP900', 'PP901', 'PP902', 'PP903', 'PP904', 'PP905', 'PP906', 'PP907', 'PP908', 'PP909', 'PP910', 'PP911', 'PP912', 'PP913', 'PP914', 'PP915', 'PP916', 'PP917', 'PP918', 'PP919', 'PP920', 'PP921', 'PP922', 'PP923', 'PP924', 'PP925', 'PP926', 'PP927', 'PP928', 'PP929', 'PP930', 'PP931', 'PP932', 'PP933', 'PP934', 'PP935', 'PP936', 'PP937', 'PP938', 'PP939', 'PP940', 'PP941', 'PP942', 'PP943', 'PP944', 'PP945', 'PP946', 'PP947', 'PP948', 'PP949', 'PP950', 'PP951', 'PP952', 'PP953', 'PP954', 'PP955', 'PP956', 'PP957', 'PP958', 'PP959', 'PP960', 'PP961', 'PP962', 'PP963', 'PP964', 'PP965', 'PP966', 'PP967', 'PP968', 'PP969', 'PP970', 'PP971', 'PP972', 'PP973', 'PP974', 'PP975', 'PP976', 'PP977', 'PP978', 'PP979', 'PP980', 'PP981', 'PP982', 'PP983', 'PP984', 'PP985', 'PP986', 'PP987', 'PP988', 'PP989', 'PP990', 'PP991', 'PP992', 'PP993', 'PP994', 'PP995', 'PP996', 'PP997', 'PP998', 'PP999']
-            , key="participante")
+
         
         if st.session_state["participante"] == "Outros":
             st.text_input("Indique os PPs", key="pp_custom", placeholder='Neste formato: PP01, PP02')
@@ -304,7 +369,7 @@ if tab_option == "Formulário":
 
 
         # Campo de Status com callback (supondo que a função update_status_fields esteja definida)
-        opts = ["Selecione um Status","PENDENTE","REALIZADO DURANTE A CONDUÇÃO", "REALIZADO", "NÃO APLICÁVEL"]
+        opts = ["PENDENTE","REALIZADO DURANTE A CONDUÇÃO", "REALIZADO", "NÃO APLICÁVEL"]
         key = "status"
 
         def _norm(x):
@@ -324,7 +389,9 @@ if tab_option == "Formulário":
             "Status",
             opts,
             key=key,
-            on_change=update_status_fields
+            on_change=update_status_fields,
+            index=None,
+            placeholder="Selecione um Status"
         )
         
 
@@ -348,7 +415,7 @@ if tab_option == "Formulário":
         
         if submit:
             # Validação dos campos obrigatórios
-            if selected_protocol == "Digite o codigo do estudo" or participante.strip() == "" or apontamento.strip() == "" or responsavel.strip() == "":
+            if selected_protocol == "Digite o codigo do estudo" or participante.strip() == "" or apontamento.strip() == "":
                 st.error("Por favor, preencha os campos obrigatórios: Código do Estudo, Participante, Responsável e Apontamento.")
             elif status == "VERIFICANDO" and verificador_nome.strip() == "":
                 st.error("Somente o Guilherme Gonçalves pode usar esse status!.")
@@ -356,8 +423,6 @@ if tab_option == "Formulário":
                 st.error("Por favor, defina um status antes de submeter o apontamento!")
             elif status == "NÃO APLICÁVEL" and justificativa.strip() == "":
                 st.error("Por favor, preencha o campo 'Justificativa'!")
-            elif responsavel == "Selecione um colaborador":
-                st.warning("Por favor, selecione o colaborador responsável antes de salvar.")
                 st.stop()
             elif correcao == "Selecione um colaborador":
                 st.warning("Por favor, selecione o colaborador responsável pela correção antes de salvar.")
@@ -372,6 +437,8 @@ if tab_option == "Formulário":
 
                 # Usa o ID gerado previamente para este apontamento
                 next_id = st.session_state.get("generated_id")
+
+                responsavel_nome = st.session_state.get("display_name")
                 
                 
 
@@ -380,7 +447,7 @@ if tab_option == "Formulário":
                     "Código do Estudo": selected_protocol,
                     "Nome da Pesquisa": research_name,
                     "Data do Apontamento": data_atual,
-                    "Responsável Pelo Apontamento": responsavel,
+                    "Responsável Pelo Apontamento": responsavel_nome,
                     "Origem Do Apontamento": st.session_state["origem"],
                     "Documentos": documento_final,  # Aqui utiliza o valor final (customizado se "Outros")
                     "Participante": pp_final,
@@ -452,12 +519,45 @@ if tab_option == "Lista de Apontamentos":
 
     df_filtrado = df.copy()
 
-    campo_id = st.text
-    opcoes_estudos = ["Todos"] + sorted(df["Código do Estudo"].dropna().unique().tolist())
-    estudo_sel = st.selectbox("Selecione o Estudo", options=opcoes_estudos)
+    st.markdown("")
 
+
+        # 🔎 Filtro por ID (linha inteira)
+    id_busca = st.text_input(
+        "Buscar por ID",
+        placeholder="Digite o ID",
+    )
+
+    if id_busca:
+        df_filtrado = df_filtrado[
+            df_filtrado["ID"].astype(str).str.contains(id_busca, case=False, na=False)
+        ]
+
+    # Linha com 2 colunas: Estudo (esquerda) e Status (direita)
+    col_filtro_estudo, col_filtro_status = st.columns(2)
+
+        # Linha com 2 colunas: Estudo (esquerda) e Status (direita)
+    col_filtro_estudo, col_filtro_status = st.columns(2)
+
+    with col_filtro_estudo:
+        opcoes_estudos = ["Todos"] + sorted(
+            df["Código do Estudo"].dropna().unique().tolist()
+        )
+        estudo_sel = st.selectbox("Selecione o Estudo", options=opcoes_estudos)
+
+    with col_filtro_status:
+        opcoes_status = ["Todos"] + sorted(
+            df["Status"].dropna().unique().tolist()
+        )
+        status_sel = st.selectbox("Filtrar por Status", options=opcoes_status)
+
+    # Aplica filtros
     if estudo_sel != "Todos":
         df_filtrado = df_filtrado[df_filtrado["Código do Estudo"] == estudo_sel]
+
+    if status_sel != "Todos":
+        df_filtrado = df_filtrado[df_filtrado["Status"] == status_sel]
+
 
     # Colunas visíveis (ID primeiro)
     cols_display = [
